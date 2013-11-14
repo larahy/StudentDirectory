64ddb3ddeb495dc0263a2578e57c75694d272f54
larahy <larahy@gmail.com>
2013-11-14 15:22:31 +0000

    Data arranged in hashes

--- directory.rb
+++ directory.rb
@@ -1,26 +1,26 @@
 students = [
-"Tom Groombridge",
-"Hannah Knights",
-"Georgi Georgiev",
-"Me!",
-"Tom Shacham",
-"Kumy Veluppillai",
-"Jean-Baptiste Blanc",
-"Nicky Keszler",
-"James Graham",
-"Bruce Steedman",
-"Michael Fisher",
-"Peter Kristo",
-"Jeremy Marer",
-"Giacomo Patella",
-"Gianni Guitteaud",
-"Asta Bevainyte",
-"Erica Salvaneschi",
-"Nisar Tahir",
-"Anath abensour",
-"Simon Woolf",
-"James Brooke",
-"Ken Scott"
+{:name => "Tom Groombridge", :cohort => :November},
+{:name => "Hannah Knights", :cohort => :November},
+{:name => "Georgi Georgiev", :cohort => :November},
+{:name => "Me!", :cohort => :November},
+{:name => "Tom Shacham", :cohort => :November},
+{:name => "Kumy Veluppillai", :cohort =>:November},
+{:name => "Jean-Baptiste Blanc", :cohort => :November},
+{:name => "Nicky Keszler", :cohort => :November},
+{:name => "James Graham", :cohort => :November},
+{:name => "Bruce Steedman", :cohort => :November},
+{:name => "Michael Fisher", :cohort => :November},
+{:name => "Peter Kristo", :cohort => :November},
+{:name => "Jeremy Marer", :cohort => :November},
+{:name => "Giacomo Patella", :cohort => :November},
+{:name => "Gianni Guitteaud", :cohort => :November},
+{:name => "Asta Bevainyte", :cohort => :November},
+{:name => "Erica Salvaneschi", :cohort => :November},
+{:name => "Nisar Tahir", :cohort => :November},
+{:name => "Anath abensour", :cohort => :November},
+{:name => "Simon Woolf", :cohort => :November},
+{:name => "James Brooke", :cohort => :November},
+{:name => "Ken Scott", :cohort => :November}
 ]
 # This is a method called print header which includes the text that heads up our script
 def print_header
@@ -29,9 +29,9 @@ def print_header
 end 
 
 # The body of our script is a method called 'print' with the argument 'names'.  The .each command takes each of the 'names' values and puts them to the screen. 
-def print(names)
-  names.each do |x|
-    puts x
+def print(students)
+  students.each do |x|
+    puts "#{x[:name]} (#{x[:cohort]} cohort)"
   end
 end 
 
